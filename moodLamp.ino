d4fcac76c480cee2263e480a2feb00a1f32c4936
kutmicro2017 <32184745+kutmicro2017@users.noreply.github.com>
2017-12-04 00:17:02 +0900

    무드등 최종 소스코드

--- moodLamp.ino
+++ moodLamp.ino
@@ -4,23 +4,17 @@
   #include <avr/power.h>
 #endif
 
-#define _DEBUG
-
 enum eColor { RED_TO_BLUE, ORANGE_TO_PURPLE, YELLOW_TO_CYAN };
 
-#define NEO_PIXEL 4
-#define COLOR_BUTTON 2
-#define BRIGHTNESS_BUTTON 3
-#define TEMP_SENSOR A5
-#define BATTERY A0
-#define RGB_RED 5
-#define RGB_GREEN 6
-#define RGB_BLUE 7
-
-#define LOWEST_TEMPERATURE 0
-#define NORMAL_TEMPERATURE 15
-#define HIGHEST_TEMPERATURE 30
-const int NORMAL_TEMPERATURE_VARIABLE = NORMAL_TEMPERATURE * 10;
+#define TEMP_SENSOR           A5
+#define BATTERY               A0
+#define COLOR_BUTTON          2
+#define BRIGHTNESS_BUTTON     3
+#define NEO_PIXEL             4
+#define RGB_RED               5
+#define RGB_GREEN             6
+#define RGB_BLUE              7
+const int NORMAL_TEMPERATURE = 15 * 10;
 const int THERMISTER = 4275;
 const int R0 = 100000; // 100kelvin
 
@@ -58,31 +52,37 @@ void setup()
   pinMode(RGB_RED, OUTPUT);
   pinMode(RGB_GREEN, OUTPUT);
   pinMode(RGB_BLUE, OUTPUT);
-
+  
   Serial.begin(9600);
 }
 
 void loop()
 {
+  //기준 : 최고 온도 30도, 중간 온도 15도, 최저 온도 0도
+  
   //To calculate the temperature
   int analogVal = analogRead(TEMP_SENSOR);
   float R = 1023.f / analogVal - 1.f;
   R *= R0;
-  float temperature = (1.f / (log(R / R0) / THERMISTER + 1 / 298.15f) - 273.15f) * 10.f;
+  float temperature = 1.f / (log(R / R0) / THERMISTER + 1 / 298.15f) - 273.15f;
 
-  int val = analogRead(BATTERY);
-  if(val < 310)
+  //To check battery level
+  int batteryVal = analogRead(BATTERY);
+  //0% ~ 30%
+  if (batteryVal < 310)
   {
     analogWrite(RGB_RED, 255);
     analogWrite(RGB_GREEN, 0);
     analogWrite(RGB_BLUE, 0);
   }
-  else if(val < 325)
+  //30% ~ 65%
+  else if (batteryVal < 325)
   {
     analogWrite(RGB_RED, 128);
     analogWrite(RGB_GREEN, 128);
     analogWrite(RGB_BLUE, 0);
   }
+  //65% ~ 100%
   else
   {
     analogWrite(RGB_RED, 0);
@@ -90,13 +90,8 @@ void loop()
     analogWrite(RGB_BLUE, 0);
   }
   
-#ifdef _DEBUG
-  Serial.print("Temp : ");
-  Serial.println(temperature / 10.f);
-#endif
-
   //To set RGB
-  RGB curr = { 0 };
+  static RGB curr = { 0 };
   static RGB prev = { 0 };
   prev = curr;
   switch (gColor)
@@ -104,49 +99,49 @@ void loop()
   case eColor::RED_TO_BLUE:
     //RED : (255, 0, 0)
     //BLUE : (0, 0, 255)
-    if (temperature >= NORMAL_TEMPERATURE_VARIABLE)
+    if (temperature <= NORMAL_TEMPERATURE)
     {
-      curr.R = 255;
-      curr.G = 255 - 1.7f * (temperature - NORMAL_TEMPERATURE_VARIABLE);
-      curr.B = 255 - 1.7f * (temperature - NORMAL_TEMPERATURE_VARIABLE);
+      curr.R = 255 - 1.7f * (NORMAL_TEMPERATURE - temperature);
+      curr.G = 255 - 1.7f * (NORMAL_TEMPERATURE - temperature);
+      curr.B = 255;
     }
     else
     {
-      curr.R = 255 - 1.7f * (NORMAL_TEMPERATURE_VARIABLE - temperature);
-      curr.G = 255 - 1.7f * (NORMAL_TEMPERATURE_VARIABLE - temperature);
-      curr.B = 255;
+      curr.R = 255;
+      curr.G = 255 - 1.7f * (temperature - NORMAL_TEMPERATURE);
+      curr.B = 255 - 1.7f * (temperature - NORMAL_TEMPERATURE);
     }
     break;
   case eColor::ORANGE_TO_PURPLE:
     //ORANGE : (255, 50, 0)
     //PURPLE : (150, 0, 220)
-    if (temperature >= NORMAL_TEMPERATURE_VARIABLE)
+    if (temperature <= NORMAL_TEMPERATURE)
     {
-      curr.R = 255;
-      curr.G = 255 - 1.4f * (temperature - NORMAL_TEMPERATURE_VARIABLE);
-      curr.B = 255 - 1.7f * (temperature - NORMAL_TEMPERATURE_VARIABLE);
+      curr.R = 255 - 0.7f * (NORMAL_TEMPERATURE - temperature);
+      curr.G = 255 - 1.7f * (NORMAL_TEMPERATURE - temperature);
+      curr.B = 255 - 0.23f * (NORMAL_TEMPERATURE - temperature);
     }
     else
     {
-      curr.R = 255 - 0.7f * (NORMAL_TEMPERATURE_VARIABLE - temperature);
-      curr.G = 255 - 1.7f * (NORMAL_TEMPERATURE_VARIABLE - temperature);
-      curr.B = 255 - 0.23f * (NORMAL_TEMPERATURE_VARIABLE - temperature);
+      curr.R = 255;
+      curr.G = 255 - 1.4f * (temperature - NORMAL_TEMPERATURE);
+      curr.B = 255 - 1.7f * (temperature - NORMAL_TEMPERATURE);
     }
     break;
   default:
     //YELLOW : (255, 255, 0)
     //CYAN : (0, 255, 255)
-    if (temperature >= NORMAL_TEMPERATURE_VARIABLE)
+    if (temperature <= NORMAL_TEMPERATURE)
     {
-      curr.R = 255;
+      curr.R = 255 - 1.7f * (NORMAL_TEMPERATURE - temperature);
       curr.G = 255;
-      curr.B = 255 - 1.7f * (temperature - NORMAL_TEMPERATURE_VARIABLE);
+      curr.B = 255;
     }
     else
     {
-      curr.R = 255 - 1.7f * (NORMAL_TEMPERATURE_VARIABLE - temperature);
+      curr.R = 255;
       curr.G = 255;
-      curr.B = 255;
+      curr.B = 255 - 1.7f * (temperature - NORMAL_TEMPERATURE);
     }
     break;
   }
@@ -183,4 +178,3 @@ void ChangeBrightness()
   }
   gbIsBrightnessChanged = true;
 }
-
